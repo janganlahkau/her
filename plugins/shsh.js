7d24c131ee2289ecdf8853ef32331ea015f00d9d
janganlahkau <81816975+janganlahkau@users.noreply.github.com>
2021-12-04 17:11:33 +0700

    Update shsh.js

--- plugins/shsh.js
+++ plugins/shsh.js
@@ -1,35 +1,33 @@
-
-fs = require('fs')
 let fetch = require('node-fetch')
+let fs = require('fs')
 let { Presence } = require('@adiwajshing/baileys')
 module.exports = {
     async all(m) {
  
 try {
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+
    if (m.isBaileys && m.fromMe) return false
+        
         let groups = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
     const type = Object.keys(m.message)[0]
     const isMedia = m.text	
-    if (isMedia) 
-{
 let dbcot = JSON.parse(fs.readFileSync('./plugins/bacot.json'))
 
                         const kata = dbcot[Math.floor(Math.random()*dbcot.length)];
          
-                   
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+    if (isMedia) 
+{
+
  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(isMedia), lc: "id" }, ''))
   let json = await res.json()
   if (!json.success) throw json
-  let result = json.success.replace(/simi/gim,  'gw').replace(/pantek/gi,  kata).replace(/simsimi/gi,  gw)
- const ran = [result,null]
-     const dom = ran[Math.floor(Math.random()*ran.length)];
-   if (result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.')
-
-await sleep(10000)
-     m.reply(dom)
+  let result = json.success.replace(/simi/gim,  'gw').replace(/pantek/gi,  kata).replace(/simsimi/gi,  'gw')
+   if (result) 
+   await sleep(7000)
+    m.reply(result)
 conn.updatePresence(m.chat, Presence.composing)
 conn.chatRead(m.chat).catch(() => { })
             }
